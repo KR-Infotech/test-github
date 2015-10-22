3b7ab205b990585489edead3feeb067ab3f02542
TheR3x <the_t-rex9836@hotmail.com>
2015-10-22 21:52:30 +0800

    Fixed Rex Pic
    HELLO

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ Nicholas Lui
 <p>Hailing 84536143<p>
 Rex the Dinosaur
 <br>
-<img src="IMG_0364.jpg" style="width: 100000000px; height:100000000000px">
+<img src="IMG_0364.jpg" style="width: 300px; height:180px">
 
 <p>Hailing 84536143<p>
 
