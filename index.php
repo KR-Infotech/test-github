119300a7fdc7ecc4e6288840a30bcdfc2ef3fd22
sugi-erifin <erifin.sugiarto@hotmail.com>
2015-10-22 22:01:55 +0800

    Sugi

--- index.php
+++ index.php
@@ -2,14 +2,13 @@ Nicholas Lui
 <br>
 <img src="nic.jpeg" style="width: 300px; height:180px">
 <p>Hazmei 97592249<p>
-<img src="http://steamykitchen.com/wp-content/uploads/2009/08/hainanese-chicken-86.jpg">
 <img src="https://pbs.twimg.com/profile_images/645209966870003712/hcu4o4___400x400.jpg">
 <p>Hailing 84536143<p>
 Rex the Dinosaur
 <br>
 <img src="IMG_0364.jpg" style="width: 300px; height:180px">
 
-<p>Hailing 84536143 Laksa<p>
+<p>Hailing 84536143<p>
 
 Ng Kai Sing 91126669
 Salted Fish
