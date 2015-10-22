476b4d196b9d5cb9ac17a82aed805b0062d54cfa
Hazmei Abdul Rahman <hazmei@live.com>
2015-10-22 21:32:18 +0800

    Hazmei
    Hazmei

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
 Nicholas Lui
 <br>
 <img src="nic.jpeg" style="width: 300px; height:180px">
+Hazmei 97592249
+<br>
+<img src="https://pbs.twimg.com/profile_images/645209966870003712/hcu4o4___400x400.jpg">
