5276fda9c67c7bae5caea7e5d16de4236af9161d
Hazmei Abdul Rahman <hazmei@live.com>
2015-10-22 22:01:26 +0800

    moved my face below my food

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ Nicholas Lui
 <img src="nic.jpeg" style="width: 300px; height:180px">
 <p>Hazmei 97592249<p>
 <img src="http://steamykitchen.com/wp-content/uploads/2009/08/hainanese-chicken-86.jpg">
+<br>
 <img src="https://pbs.twimg.com/profile_images/645209966870003712/hcu4o4___400x400.jpg">
 <p>Hailing 84536143<p>
 Rex the Dinosaur
