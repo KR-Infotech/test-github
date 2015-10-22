a9132feb4397130a66cb683b6ffaa61cc7c2ed60
Hazmei Abdul Rahman <hazmei@live.com>
2015-10-22 21:59:59 +0800

    MY FAVOURITE FOOD

--- index.php
+++ index.php
@@ -2,6 +2,7 @@ Nicholas Lui
 <br>
 <img src="nic.jpeg" style="width: 300px; height:180px">
 <p>Hazmei 97592249<p>
+<img src="http://steamykitchen.com/wp-content/uploads/2009/08/hainanese-chicken-86.jpg">
 <img src="https://pbs.twimg.com/profile_images/645209966870003712/hcu4o4___400x400.jpg">
 <p>Hailing 84536143<p>
 Rex the Dinosaur
